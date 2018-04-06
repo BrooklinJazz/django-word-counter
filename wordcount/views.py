56432a1e5f9e17b9da92331dfe10fc9783ab181d
Brooklin Myers <Brooklinjazz@hotmail.com>
2018-04-06 13:58:23 -0700

    use operator.itemgetter to show how to order a dictionary

--- wordcount/views.py
+++ wordcount/views.py
@@ -1,5 +1,6 @@
 from django.http import HttpResponse
 from django.shortcuts import render
+import operator
 
 def home(request):
     return render(request, 'home.html')
@@ -18,5 +19,7 @@ def count(request):
         else:
             worddictionary[word] = 1
 
+    sortedwords = sorted(worddictionary.items(), key=operator.itemgetter(1), reverse=True )
 
-    return render(request, 'count.html', {'fulltext':fulltext, 'count': len(wordlist), 'worddictionary': worddictionary.items})
+
+    return render(request, 'count.html', {'fulltext':fulltext, 'count': len(wordlist), 'sortedwords': sortedwords})
